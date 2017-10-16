7cb945d9b2621da1647449ffd9ef6616e76ed560
Trevor Creed <trevor.creed@libertymutual.com>
2017-10-16 15:29:43 -0400

    Remove jquery code, vue handles adding, saving, clearing students, also computing grade.

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-Vue.component('student-item', {
-  props: ['id', 'name', 'score', 'grade'],
-  template: `<li>
-      <input class='student-name' type='text' placeholder='Name' v-model='name'></input>
-      <input class='student-score' type='text' placeholder='Score' v-model='score'></input>
-      <span class='student-grade'>{{grade}}</span>
-      </li>`
-})
-
 var app = new Vue({
   el: '#app',
   data: {
-    students: [{
-      id: '1234',
-      name: 'Test Name',
-      score: 75,
-      grade: 80
+    students: JSON.parse(localStorage.getItem('students')) || [{
+      id: guid(),
+      name: '',
+      score: ''
     }]
+  },
+  methods: {
+    calculateGrade(x, y=1.42, z=60) {
+      if (Number.parseFloat(x) || x === 0 || x === '0')
+        return (Math.pow(Math.max(0, x-z) / (100-z), 1/y) * (100-z) + z).toPrecision(4)
+      return ''
+    },
+    addStudent(){
+      this.students.push({
+        id: guid(),
+        name: '',
+        score: ''
+      })
+    },
+    saveStudents(){
+      localStorage.setItem('students', JSON.stringify(this.students))
+    },
+    clearStudents(){
+      this.students.splice(0, this.students.length)
+      localStorage.removeItem('students')
+    }
   }
 })
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1)
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4()
+}
